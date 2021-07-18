7c92cd12349c622f9929142028bc108f017c0093
Moskzow <inmoskwetrust@gmail.com>
2021-07-18 16:22:14 +0000

    ex 3 is done

--- exercises/03.2-Retrieve-Items/app.js
+++ exercises/03.2-Retrieve-Items/app.js
@@ -1,3 +1,8 @@
 var arr = [4,5,734,43,45,100,4,56,23,67,23,58,45,3,100,4,56,23];
 
 //your code here
+let fisrtElement = arr[0];
+console.log(fisrtElement);
+
+let fourthElement = arr[3];
+console.log(fourthElement);
